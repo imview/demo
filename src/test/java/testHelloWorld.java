e1996eefa688d5be1f84999a2c86e626bbe5f7ea
imview <13751732974@163.com>
2019-11-21 10:05:31 +0800

    Update testHelloWorld.java

--- src/test/java/testHelloWorld.java
+++ src/test/java/testHelloWorld.java
@@ -4,6 +4,8 @@ public class testHelloWorld {
     @Test
     public void helloWorld(){
         System.out.println("Hello World!");
-        System.out.println("demo inti success!");
+        System.out.println("demo init success!");
+        System.out.println("master change!");
+
     }
 }
